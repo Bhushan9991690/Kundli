f31209083e7ef544c5276bf8dfb55a3016400765
bhushan <godwaha2003@gmail.com>
2025-02-05 14:06:39 +0530

    Adding dotenv feature

--- config/database.js
+++ config/database.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
+
 const connectDB = async () => {
-  // const db_password=ln3uLs54q9C34zJN;
-  // const db_User=godwaha2003;F
   await mongoose.connect(
-    "mongodb+srv://godwaha2003:ln3uLs54q9C34zJN@kundlimatch.y6ivg.mongodb.net/?retryWrites=true&w=majority&appName=kundliMatch"
+    `mongodb+srv://${process.env.db_User}:${process.env.db_password}@kundlimatch.y6ivg.mongodb.net/?retryWrites=true&w=majority&appName=kundliMatch`
   );
 };
 module.exports = connectDB;
