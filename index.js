311dde2bf7e6710333fe5e18ceaa86fa2544ea12
bhushan <godwaha2003@gmail.com>
2025-02-05 14:23:16 +0530

    ..

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 require("dotenv").config();
 const connectDB = require("./config/database");
 const app = express();
+const PORT=process.env.port;
 connectDB()
   .then(() => {
     console.log("Conneted successfully to DB");
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
       console.log("Listening server 3000");
     });
   })
